e63f39ba1819aaf6064533cabd84c472dcd49786
Christian Walker <cwal7887@gmail.com>
2019-09-17 09:57:41 -0400

    M2T1 Two Integer Addition
    09/17/2019

---
+++ M2T1_Walker.py
@@ -0,0 +1,29 @@
+#M2T1
+#Christian Walker
+#9/17/2019
+#Two Integer Addition
+
+#Declare variables
+#Declare integer i1
+#Declare integer i2
+
+#Display Calculator Instructions
+print("This program can only add two WHOLE numbers")
+
+#Display "Enter First Interger" then input i1
+i1 = int(input("Enter First Integer: "))
+
+#Display "Enter Second Interger" the input i2
+i2 = int(input("Enter Second Integer: "))
+
+#Add i1 and i2 to get Sum
+Sum = i1 + i2
+
+#Display i1 + i2
+print(i1, "+", i2, "is equal to...")
+
+#Display Sum
+print(Sum)
+
+input("Press any key to continue...")
+
